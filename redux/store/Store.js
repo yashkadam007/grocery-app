8d46baf6ab1f6a65a5ef4c7f0cc186eca895cb6e
Yash Kadam <yash@Yashs-MacBook-Air.local>
2024-02-01 01:17:41 +0530

    feat: removed white screen bug

--- redux/store/Store.js
+++ redux/store/Store.js
@@ -1,13 +1,26 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import cartReducer from "../reducer/Reducers";
-import  {thunk} from "redux-thunk";
-import favoriteReducer from "../reducer/FavouriteReducers";
+// import { createStore, combineReducers, applyMiddleware } from "redux";
+// import cartReducer from "../reducer/Reducers";
+// import  { thunk } from "redux-thunk";
+// import favoriteReducer from "../reducer/FavouriteReducers";
 
-const rootReducer = combineReducers({
-  cart: cartReducer,
+// const rootReducer = combineReducers({
+//   cart: cartReducer,
+//   favorite: favoriteReducer,
+// });
+
+// const store = createStore(rootReducer, applyMiddleware(thunk));
+
+// export default store;
+import { configureStore} from '@reduxjs/toolkit';
+import cartReducer from '../reducer/Reducers';
+import favoriteReducer from '../reducer/FavouriteReducers';
+
+
+const store = configureStore({
+  reducer:{
+    cart: cartReducer,
   favorite: favoriteReducer,
+  }
 });
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
